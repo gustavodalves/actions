b7aa95383b0089e5d4c4f50a3501e4ce07a76bdc
Gustavo Duarte ALves <gualves2001@icloud.com>
2024-01-06 13:32:07 -0300

    ci: test to fail

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -2,7 +2,7 @@ import sum from "./sum"
 
 describe("sum", () => {
     it("shoud be able do sum", () => {
-        const result = sum(1,2,3)
+        const result = sum(1,2)
 
         expect(result).toBe(6)
     })
